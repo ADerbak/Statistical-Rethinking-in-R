d456845a69f97076fab8b9fd569058665271fe01
Andrew Derbak <andrewderbak@gmail.com>
2021-01-05 14:15:59 -0600

    Initial commit. Contains initial working document.

---
+++ Statistical Rethinking.r
@@ -0,0 +1,12 @@
+########
+# Author: Andrew Derbak
+# *Statistical Rethinking* Notes
+# from the book *Statistical Rethinking* by Roger McElreath
+#########
+
+
+# Install rethinking packages
+
+install.packages(c("coda","mvtnorm","devtools","daggity"))
+library(devtools)
+devtools::install_github("rmcelreath/rethiking")
